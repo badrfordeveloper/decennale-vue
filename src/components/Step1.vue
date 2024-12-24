27cafb62fc6b2dc73c5669545e60e9714272b343
badreddine jeddab <badreddine jeddab>
2024-12-24 16:35:44 +0100

    api siren + loader

--- src/components/Step1.vue
+++ src/components/Step1.vue
@@ -51,14 +51,18 @@
             </div >
             <div class="col-12 ">
 
-                <label class="formLabel mb-2"> Rechercher mon entreprise (nom, SIREN, SIRET) </label>
+                <label class="formLabel mb-2"> Num de SIREN : </label>
                 <input type="text"  v-model="data.siren" class="form-control ">
                 <ErrorComponent v-if="$v.siren.$error" :errors="$v.siren.$errors" />
             </div >
    
 
             <div class="col-12">
-                <button type="submit" class="navBtn firstBtn mt-4">Votre devis en 2 minutes</button>
+                <button v-if="loader" type="button" class="navBtn nextBtn mt-4 d-flex justify-content-center align-items-center">
+                                <vue-spinner size="30" color="white" />
+                            </button>
+
+                <button v-else type="submit" class="navBtn firstBtn mt-4">Votre devis en 2 minutes</button>
             </div>
         </div>
   
@@ -71,7 +75,11 @@ import { useFormStore } from '@/stores/useFormStore';
 import { reactive, ref } from 'vue';
 import useVuelidate from '@vuelidate/core';
 import { required, email } from '@vuelidate/validators';
+import axios from 'axios';
+import {VueSpinner} from 'vue3-spinners';
+
 const formStore = useFormStore();
+const loader= ref(false)
 
 const types = ["Auto Entrepreneur","Nom Propre","SARL","SA","SAS,SASU..." ];
 
@@ -91,19 +99,47 @@ const rules = {
 
     const $v = useVuelidate(rules, data);
 
-const showModal = ref(false);
-const modalTrigger = ref(null);
-function submitStep() {
-   /*  if (data.profession === "AUTRE") {
-        showModal.value = true;
-        modalTrigger.value?.click();
-        return;
-    } */
+async function submitStep() {
+
    
     $v.value.$touch(); // Mark all fields as touched
     if (!$v.value.$invalid) {
+        if(data.siren != formStore.formData.step1.siren){
+            loader.value =true;
+            await axios.get('https://api.insee.fr/api-sirene/3.11/siret?q=siren:'+data.siren,{
+            headers: {
+            'X-INSEE-Api-Key-Integration': `28774ea5-fd7e-442a-b74e-a5fd7ec42af9`, // Add token to header
+            }})
+            .then(response => {
+                if (response.status === 200) {
+                    console.log(response)
+                    const etablissement = response.data.etablissements[0];
+                    const infosStep2 = {
+                        nom_entreprise: etablissement.uniteLegale.denominationUniteLegale, //
+                        type: formStore.formData.step2.type || "",
+                        adresse:  etablissement.adresseEtablissement.numeroVoieEtablissement + " "+ etablissement.adresseEtablissement.typeVoieEtablissement,
+                        code_postal:  etablissement.adresseEtablissement.codePostalEtablissement,//
+                        ville:  etablissement.adresseEtablissement.libelleCommuneEtablissement,//
+                        mobile: formStore.formData.step2.mobile || "",
+                        mail: formStore.formData.step2.mail || "",
+                        nombre_salaries: formStore.formData.step2.nombre_salaries || "",
+                        chiffre_affaires: formStore.formData.step2.chiffre_affaires || "",
+                        date_creation: etablissement.dateCreationEtablissement,//
+                        nom_dirigeant: formStore.formData.step2.nom_dirigeant || "",
+                        date_naissance: formStore.formData.step2.date_naissance || "",
+                    }
+                    formStore.updateStepData('step2', infosStep2)
+                    // update before you go
+            
+                }
+            }).catch(({response}) => {
+            }).finally(() => {
+                loader.value =false;
+            });
+        }
         formStore.updateStepData('step1', data);
         formStore.nextStep();
+ 
     }
 }
 </script>
